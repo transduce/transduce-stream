7d26d88deff20a67ca7cb624f43b34f8f38a50fa
Kevin Beaty <kevin@simplectic.com>
2014-10-14 19:48:13 -0500

    Add destroy from through2 and _r.pipestdio that waits for flush before exit

--- transduce-stream.js
+++ transduce-stream.js
@@ -28,15 +28,36 @@ TransduceStream.prototype._flush = function(cb){
 function transformCallback(stream, transducer){
   return _r(null, transducer)
     .each(function(item){stream.push(item)})
-    .asyncCallback(function(err){
-      if(err){
-        stream.emit('error', err);
-      }
-      stream.end();
-    });
+    .asyncCallback(stream.destroy.bind(stream));
+}
+
+// from https://github.com/rvagg/through2
+TransduceStream.prototype.destroy = function(err){
+  if(this._destroyed) return;
+  this._destroyed = true;
+
+  var self = this;
+  process.nextTick(function(err){
+    if(err) self.emit('error', err);
+    self.emit('close');
+  });
 }
 
 _r.stream = TransduceStream;
 _r.prototype.stream = function(options){
   return _r.stream(this, options);
+}
+
+// Helper to pipe stream from stdin to stdout
+// and wait for stdout to flush before exit
+// stream is any node.js stream (use _r.stream to create)
+_r.pipestdio = function(stream){
+  process.stdin.pipe(stream).pipe(process.stdout);
+  process.stdin.resume();
+
+  process.on('exit', function(){
+    process.stdout.once('drain', function(){
+      process.exit(0);
+    });
+  });
 }
