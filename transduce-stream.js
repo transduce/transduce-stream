25c722875c00c99ca471f9672f25e621319cce3e
Kevin Beaty <kevin@simplectic.com>
2014-10-17 22:41:22 -0500

    Change dep to tranduce/push and move to transduce/stream

--- transduce-stream.js
+++ transduce-stream.js
@@ -1,9 +1,10 @@
-var _r = require('transduce-string'),
+var compose = require('transduce-protocol').compose,
+    tp = require('transduce-push'),
     util = require('util'),
     Transform = require('stream').Transform,
     transform, stream;
 
-module.exports = _r;
+module.exports = TransduceStream;
 
 util.inherits(TransduceStream, Transform);
 function TransduceStream(transducer, options){
@@ -26,9 +27,8 @@ TransduceStream.prototype._flush = function(cb){
 }
 
 function transformCallback(stream, transducer){
-  return _r(null, transducer)
-    .each(function(item){stream.push(item)})
-    .asyncCallback(stream.destroy.bind(stream));
+  var xf = compose(transducer, tp.each(function(item){stream.push(item)}));
+  return tp.asyncCallback(xf, stream.destroy.bind(stream));
 }
 
 // from https://github.com/rvagg/through2
@@ -40,24 +40,5 @@ TransduceStream.prototype.destroy = function(err){
   process.nextTick(function(err){
     if(err) self.emit('error', err);
     self.emit('close');
-  });
-}
-
-_r.stream = TransduceStream;
-_r.prototype.stream = function(options){
-  return _r.stream(this, options);
-}
-
-// Helper to pipe stream from stdin to stdout
-// and wait for stdout to flush before exit
-// stream is any node.js stream (use _r.stream to create)
-_r.pipestdio = function(stream){
-  process.stdin.pipe(stream).pipe(process.stdout);
-  process.stdin.resume();
-
-  process.on('exit', function(){
-    process.stdout.once('drain', function(){
-      process.exit(0);
-    });
   });
 }
